b27b40aa02a761e8cac8ba37ea073878219ae6dc
luucamay <lupemaydana@gmail.com>
2020-01-22 15:43:12 -0400

    Using NeDB

--- server.js
+++ server.js
@@ -7,6 +7,29 @@ var express = require('express'),
   app = express(),
   port = Number(process.env.PORT || 8080);
 
+// DATABASE
+// ===============================================
+
+// Setup the database.
+var Datastore = require('nedb');
+var db = new Datastore({
+  filename: 'goals.db', // provide a path to the database file
+  autoload: true, // automatically load the database
+  timestampData: true // automatically add and manage the fields createdAt and updatedAt
+});
+
+// Let us check that we can save to the database.
+// Define a goal.
+var goal = {
+  description: 'Do 10 minutes meditation every day',
+};
+
+// Save this goal to the database.
+db.insert(goal, function(err, newGoal) {
+  if (err) console.log(err);
+  console.log(newGoal);
+});
+
 // ROUTES
 // ===============================================
 
